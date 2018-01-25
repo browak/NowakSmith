a51d09c6675d745276de869daa300ead82aed401
Adam Nowak <adamnowak@Adams-New-MBP.home>
2018-01-24 20:16:57 -0500

    Uploading listings data

---
+++ hedonicText/R/listings.R
@@ -0,0 +1,12 @@
+#' Transaction data with text
+#'
+#' @docType data
+#' @usage data(grav)
+#' @format A data frame.
+#' @keywords datasets
+#'
+#' @source A sample of 24,446 transactions with sale price, square footage, and publice remarks written by a real estate agent.
+#'
+#' @examples
+#' data(listings)
+#' univariateHedonic <- lm(price~sqft , data=listings)
